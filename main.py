f7e270928ba2797060e4989ee1e6be3359eb72ed
Marc-Antoine Robin <marc.antoine.robin@uout.io>
2021-11-20 08:55:40 -0500

    adding file to capture

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-import cv2
-
+from capture_from_camera import capture_from_camera
 from send_to_server import send_to_server
 
 if __name__ == '__main__':
-    frameWidth = 640
-    frameHeight = 480
-    cap = cv2.VideoCapture(0)
-    cap.set(3, frameWidth)
-    cap.set(4, frameHeight)
-    cap.set(10, 150)
+    cap = capture_from_camera()
 
     lastImage = None
     while True:
