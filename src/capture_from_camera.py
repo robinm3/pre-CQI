4eff8a65f0d3adfb3550b9585560ee0334da1259
Simon Martineau <simon.martineau.dev@gmail.com>
2021-11-20 11:20:47 -0500

    ez

--- src/capture_from_camera.py
+++ src/capture_from_camera.py
@@ -1,12 +1,15 @@
 import cv2
 
 
-def capture_from_camera():
-    frameWidth = 640
-    frameHeight = 480
-    cap = cv2.VideoCapture(0)
-    cap.set(3, frameWidth)
-    cap.set(4, frameHeight)
-    cap.set(10, 150)
+class ShapeImageCaptor():
 
-    return cap
+    def __init__(self):
+        self.queue = []
+        self._capture = cv2.VideoCapture(2)
+
+    def clear(self):
+        self.queue = []
+
+    def show_frame(self):
+        ret, frame = self._capture.read()
+        cv2.imshow('frame', frame)
--- src/main.py
+++ src/main.py
@@ -1,17 +1,29 @@
-from capture_from_camera import capture_from_camera
+import time
+import cv2
+
+from capture_from_camera import VideoFeed
 from send_to_server import ServerCommunicator
 from shape_detector import detect_shape
 
 if __name__ == '__main__':
 
     server = ServerCommunicator()
-    cap = capture_from_camera()
+    
+    video_feed = VideoFeed()
+
+    while(1):
+        video_feed.show_frame()
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
 
     lastImage = None
     shapeList = []
 
     while True:
         success, img = cap.read()
+        cv2.imshow('img', img)
+        time.sleep(10)
         if success and img != lastImage:
             lastImage = img
 
