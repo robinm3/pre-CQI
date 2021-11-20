5e8e0d2eceb307892688240ac33d27cbafa0afa4
Olivier Caron <olivier.caron.12@ulaval.ca>
2021-11-20 11:18:05 -0500

    improved shape detection

--- src/shape_detector.py
+++ src/shape_detector.py
@@ -2,36 +2,41 @@ import cv2
 
 
 def detect_shape(image):
-    image = cv2.imread(image)
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    ret, threshold = cv2.threshold(gray_image, 240, 255, cv2.THRESH_BINARY)
+    _, threshold = cv2.threshold(gray_image, 240, 255, cv2.THRESH_BINARY)
     contours, _ = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
     if len(contours) == 0:
         print("circle")
         return "circle"
 
-    approx = cv2.approxPolyDP(contours[1], 0.005 * cv2.arcLength(contours[1], True), True)
+    for contour in contours:
 
-    if len(approx) == 4:
-        print("square")
-        return "square"
+        area = cv2.contourArea(contour)
 
-    elif len(approx) == 3:
-        print("croix")
-        return "croix"
+        if area > 5000:
 
-    else:
-        print("arrow")
-        return "arrow"
-        
+            approx = cv2.approxPolyDP(contour, 0.005 * cv2.arcLength(contour, True), True)
+
+            if len(approx) == 4:
+                print("square")
+                return "square"
+
+            elif len(approx) == 12:
+                print("croix")
+                return "croix"
+
+            elif len(approx) == 7:
+                print("arrow")
+                return "arrow"
+            
 
 if __name__ == '__main__':
-    detect_shape("../img/square.png")
+    detect_shape(cv2.imread("../img/square.png"))
     print("-------------------")
-    detect_shape("../img/arrow.png")
+    detect_shape(cv2.imread("../img/arrow.png"))
     print("-------------------")
-    detect_shape("../img/circle.png")
+    detect_shape(cv2.imread("../img/circle.png"))
     print("-------------------")
-    detect_shape("../img/croix.png")
+    detect_shape(cv2.imread("../img/croix.png"))
 
