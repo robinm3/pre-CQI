d2f54a783073055b80d4039e02cf64af28789eef
Marc-Antoine Robin <marc.antoine.robin@uout.io>
2021-11-20 11:19:43 -0500

    fixing similarity

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from capture_from_camera import capture_from_camera
 from send_to_server import ServerCommunicator
-from shape_detector import detect_shape
+from shape_detector import detect_shape, are_two_images_similar
 
 if __name__ == '__main__':
 
@@ -15,11 +15,12 @@ if __name__ == '__main__':
     while True:
         success, img = cap.read()
 
-        if success and not np.array_equal(img, lastImage):
+        if success and not are_two_images_similar(lastImage, img):
             lastImage = img
 
             shape = detect_shape(img)
-            shapeList.append(shape)
+            if shape:
+                shapeList.append(shape)
 
         if len(shapeList) > 4:
             server.send(shapeList)
