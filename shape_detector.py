e57094e1f0e2ab323f29ee8c535e696f8155abe1
Olivier Caron <olivier.caron.12@ulaval.ca>
2021-11-20 09:41:26 -0500

    shape detection

---
+++ shape_detector.py
@@ -0,0 +1,15 @@
+import cv2
+
+def detect_shape(image):
+    image = cv2.imread(image)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    ret, threshold = cv2.threshold(gray_image, 240, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
+    for contour in contours:
+        approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contours, True), True)
+
+        if len(approx) == 4:
+            print("square")
+
+
