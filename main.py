8212a5c5d977360e1a34173c68cf9f33a988bc27
Olivier Caron <olivier.caron.12@ulaval.ca>
2021-11-20 09:50:49 -0500

    added test for shape detection in main

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from capture_from_camera import capture_from_camera
 from send_to_server import ServerCommunicator
+from shape_detector import detect_shape
 
 if __name__ == '__main__':
+    #test, to be removed
+    detect_shape("square.png")
+
     server = ServerCommunicator()
     cap = capture_from_camera()
 
     lastImage = None
     shapeList = []
+
     while True:
         success, img = cap.read()
         if success and img != lastImage:
